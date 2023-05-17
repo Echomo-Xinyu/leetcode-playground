64e1647696848ecf8f00071c2e21525d521ab4f7
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-17 09:30:36 +0800

    reverse integer

---
+++ reverse_integer.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/reverse-integer/description/
+class Solution:
+    # slightly faster
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            return -1 * self.reverse(-1 * x)
+
+        stack = []
+        while x > 0:
+            stack.append(x % 10)
+            x //= 10
+        res = 0
+        radix = 1
+        while len(stack) > 0:
+            res += radix * stack.pop()
+            radix *= 10
+        if -2**31 <= res <= 2**31-1:
+            return res
+        else:
+            return 0
+
+    # below is accepted but slow version using reverse string
+    def _1reverse(self, x: int) -> int:
+        res = 0
+        if x < 0:
+            res = int(str(x)[1:][::-1]) * -1
+        else:
+            res = int(str(x)[::-1])
+        if -2**31 <= res <= 2**31-1:
+            return res
+        else:
+            return 0
+
+print(Solution().reverse(123))
