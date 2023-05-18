ae3a27f25b400c280aaef9b9399048e761899d39
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-18 15:00:03 +0800

    add second approach for three sum

--- three_sum.py
+++ three_sum.py
@@ -2,7 +2,31 @@
 from typing import List
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    # 2sum two pointer approach upgrade
+    def _2threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1 
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
+
+    # accepted but not good enough
+    def _1threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
         count = {}
         for i, num in enumerate(nums):
