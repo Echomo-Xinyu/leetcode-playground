26b917dcec52d66f5278813504bbbac8a7127243
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-19 17:56:43 +0800

    add note and new method

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/validate-binary-search-tree/
 from typing import Optional
+# TODO: good practice for recursion
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -8,7 +9,24 @@ class TreeNode:
         self.right = right
 
 class Solution:
+    # compute in tree-style
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node):
+            cur_min = cur_max = node.val
+            if node.left:
+                left_min, left_max, is_valid = helper(node.left)
+                if not is_valid or left_max >= node.val:
+                    return -1, -1, False
+                cur_min = left_min
+            if node.right:
+                right_min, right_max, is_valid = helper(node.right)
+                if not is_valid or right_min <= node.val:
+                    return -1, -1, False
+                cur_max = right_max
+            return cur_min, cur_max, True
+        return helper(root)[-1]
+
+    def _2isValidBST(self, root: Optional[TreeNode]) -> bool:
         def _inOrderTraversal(root: Optional[TreeNode]) -> List[TreeNode]:
             if root:
                 return _inOrderTraversal(root.left) + [root.val] + _inOrderTraversal(root.right)
