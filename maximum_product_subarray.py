c6d6883e22ade7d042d1bdf2fe73452d24b79e10
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-22 11:21:11 +0800

    add concise version

--- maximum_product_subarray.py
+++ maximum_product_subarray.py
@@ -1,7 +1,31 @@
 # https://leetcode.com/problems/maximum-product-subarray/
 from typing import List
+# TODO
 
 class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = -float("inf")
+        min_ending_here = max_ending_here = 1
+        for num in nums:
+
+            new_max_ending_here = max(
+                min_ending_here * num,
+                max_ending_here * num,
+                num
+            )
+
+            new_min_ending_here = min(
+                min_ending_here * num,
+                max_ending_here * num,
+                num
+            )
+
+            max_ending_here = new_max_ending_here
+            min_ending_here = new_min_ending_here
+            res = max(res, max_ending_here)
+        return res
+
+    # accepted but not concise enough
     def _1maxProduct(self, nums: List[int]) -> int:
         dp = [[None, None] for _ in nums]
         dp[0] = [nums[0], 0]
