dd0970478535ab145226bf3002db8af76a36ee20
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-22 11:26:55 +0800

    add alternative solution

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/reverse-linked-list/
+# TODO
 from typing import Optional
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -7,6 +8,15 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+    def _1reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return head
         stack = []
