e4346cde7e310ef36c19daa0b03a29036e34e6fb
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-23 10:20:01 +0800

    add more concise method

--- number_of_islands.py
+++ number_of_islands.py
@@ -3,6 +3,37 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid)
+        if not n:
+            return 0
+        m = len(grid[0])
+        if not m:
+            return 0
+
+        grid_copy = [[grid[i][j] for j in range(m)] for i in range(n)]
+        dxdy = ((-1, 0), (1, 0), (0, 1), (0, -1))
+                
+        def dfs(x, y):
+            grid_copy[x][y] = '0'
+            for dx, dy in dxdy:
+                x_new, y_new = x + dx, y + dy
+                if not (0 <= x_new < n and 0 <= y_new < m):
+                    continue
+                if grid_copy[x_new][y_new] == '0':
+                    continue
+                dfs(x_new, y_new)
+            return
+        
+        cnt = 0
+        for x in range(n):
+            for y in range(m):
+                if grid_copy[x][y] == '1':
+                    cnt += 1
+                    dfs(x, y)
+        return cnt
+
+    # accepted but can be more concise
+    def _1numIslands(self, grid: List[List[str]]) -> int:
         visited = [[False for _ in grid[0]] for _ in grid]
         num = 0
         frontier = []
