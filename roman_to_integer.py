6742832c3e255a3daebb4ea354341ccfac67ee7d
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-25 14:14:09 +0800

    add optimized solution

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,4 +1,25 @@
 class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbol_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        result = 0
+        prev_value = 0
+        for symbol in s:
+            value = symbol_values[symbol]
+            if value > prev_value:
+                result += value - 2 * prev_value
+            else:
+                result += value
+            prev_value = value
+        return result
+
     def _1romanToInt(self, s: str) -> int:
         mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         two_letter_mapping = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
