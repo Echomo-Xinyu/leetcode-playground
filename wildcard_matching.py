475c5714592ab3fcc754f7cfb955f969629099f9
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-25 14:00:42 +0800

    add iterative solution

--- wildcard_matching.py
+++ wildcard_matching.py
@@ -2,6 +2,24 @@
 # TODO: similar to regular expression matching
 
 class Solution:
+    # line 9-13 draw inspiration from others' solution
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False for _ in range(len(p) + 1)] for _ in range(len(s) + 1)]
+        dp[-1][-1] = True
+        for i in range(len(p)-1, -1, -1):
+            if p[i] != "*":
+                break
+            else:
+                dp[-1][i] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                if p[j] == "*":
+                    dp[i][j] = dp[i][j+1] or dp[i+1][j]
+                else:
+                    dp[i][j] = (p[j] in {s[i], "?"}) and dp[i+1][j+1]
+        return dp[0][0]
+
     # recursion solution, working but reach recursion depth limit
     def _1isMatch(self, s: str, p: str) -> bool:
         mem = {}
