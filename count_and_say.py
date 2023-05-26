9f4402f0b5a62bbaaf08aa98a62d912b4ed52a61
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-26 11:00:28 +0800

    count and say

---
+++ count_and_say.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        res = "1"
+        for i in range(1, n):
+            new_res = ""
+            curr = res[0]
+            num = 1
+            for i in range(1, len(res)):
+                if res[i] == curr:
+                    num += 1
+                else:
+                    new_res.append(str(num) + str(curr))
+                    curr = res[i]
+                    num = 1
+            new_res.append(str(num) + str(curr))
+            res = new_res
+        return res
