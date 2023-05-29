f404718745c039cbf66fdf2456ec4aff23235cbd
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-05-29 11:24:13 +0800

    permutation

---
+++ permutations.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/permutations/
+from typing import List
+# TODO: hard to code
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            # return # backtracking
+        for i in xrange(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
