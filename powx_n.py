2f983091b84cbc416dfeb244fec07635ae0b17c4
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-06-06 10:46:45 +0800

    add iterative solution

--- powx_n.py
+++ powx_n.py
@@ -2,8 +2,22 @@
 # TODO
 
 class Solution:
-    # inspired by editorial
     def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            return 1 / self.myPow(x, -1*n)
+        elif n == 0:
+            return 1.0
+        res = 1
+        while n > 0:
+            if n % 2 == 1:
+                res *= x
+                n -= 1
+            x = x * x
+            n = n / 2
+        return res
+
+    # inspired by editorial
+    def _1myPow(self, x: float, n: int) -> float:
         def _pow(x, n):
             if n == 0:
                 return 1.0
