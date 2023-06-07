286897e275f3d55591fb03f6b411c1fd4705672b
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-06-07 10:43:37 +0800

    minimum flips to make a or b equal to c

---
+++ minimum_flips_to_make_a_or_b_equal_to_c.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/
+
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        res = 0
+        while (a > 0 or b > 0) and c > 0:
+            a_digit, b_digit, c_digit = a % 2, b % 2, c % 2
+            if a_digit == 0:
+                if b_digit == 0:
+                    if c_digit == 0:
+                        pass
+                    else:
+                        res += 1
+                else:
+                    if c_digit == 0:
+                        res += 1
+                    else:
+                        pass
+            else:
+                if b_digit == 0:
+                    if c_digit == 0:
+                        res += 1
+                    else:
+                        pass
+                else:
+                    if c_digit == 0:
+                        res += 2
+                    else:
+                        pass
+            a = a // 2
+            b = b // 2
+            c = c // 2
+        while a > 0:
+            curr_digit = a % 2
+            if curr_digit == 1:
+                res += 1
+            a = a // 2
+        while b > 0:
+            curr_digit = b % 2
+            if curr_digit == 1:
+                res += 1
+            b = b // 2
+        while c > 0:
+            curr_digit = c % 2
+            if curr_digit == 1:
+                res += 1
+            c = c // 2
+        return res
