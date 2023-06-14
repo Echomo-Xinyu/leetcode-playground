900dbbb6ddcb82b876a75ba115ea83e12240ed59
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-06-14 12:23:52 +0800

    add alternative solution

--- sqrt_x.py
+++ sqrt_x.py
@@ -2,6 +2,22 @@
 # TODO: good prac with binary search
 
 class Solution:
+    # by others
+    def _mySqrt(self, x: int) -> int:
+        if x < 2: return x
+
+        left, right = 2, x // 2
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            num = mid * mid
+            if num > x:
+                right = mid - 1
+            elif num < x:
+                left = mid + 1
+            else:
+                return mid
+        return right
     # take note the boundary case
     def mySqrt(self, x: int) -> int:
         low, high = 0, x
