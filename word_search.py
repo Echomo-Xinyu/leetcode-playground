7bcc5f1736aa55abecd9b1043b5b22e17c58f296
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-06-20 10:59:39 +0800

    add complete solution

--- word_search.py
+++ word_search.py
@@ -1,9 +1,33 @@
 # https://leetcode.com/problems/word-search/
 from typing import List
+#TODO
 
 class Solution:
+    def exist(self, board: List[List[int]], word: str) -> bool:
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(borad[0])):
+                if dfs(board, i, j, word):
+                    return True
+        return False
+    
+    def dfs(board, i, j, word):
+        if len(word) == 0:
+            retur True
+        if i < 0 or i >= len(board) or j < 0 or j > len(board[0]) or word[0] != board[i][j]:
+            return False
+        tmp = board[i][j]
+        board[i][j] = "#"
+        w = word[1:]
+        res = dfs(board, i+1, j, w) or dfs(board, i-1, j, w) or dfs(board, i, j-1, w) or dfs(board, i, j+1, w)
+        board[i][j] = temp
+        return res
+
+
+
     # failed attempt
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def _1exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
         # starts
         ss = []
